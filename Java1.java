1e614fcaa79e2e072ebdfd7652f07e54ca998fae
pavandarri <45012263+pavandarri@users.noreply.github.com>
2018-11-13 23:36:16 +0530

    Add files via upload

---
+++ Java1.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public Class A{
+Public Static Void Main(){
+int i=0;
+int j=0;
+Scanner Sc=new Scanner(Scanner.in);
+String Candidate1=Sc.nextline();
+int votes1=Sc.nextint();
+String Candidate2=Sc.nextline();
+int votes2=Sc.nextint();
+if(votes1>votes2)
+{
+System.out.println("Candidate1:"+ Candidate1is the winner);
+}
+else{
+System.out.println("Candidate2:"+Candidate2 is the winner);
+}
+if(votes1=votes2)
+{
+if(Candidate1.charAt(i)<Candidate2.charAt(j));
+{
+System.out.println("Candidate2:"+Candidate2 is the winner);
+}
+else{
+System.out.println("Candidate1:"+Candidate1 is the winner
+}
+}
